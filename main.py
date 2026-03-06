2bec76f07b881c7afbb2a39c863164032e9a49c7
DiegoMy7 <1505613@senati.pe>
2026-03-06 10:11:57 -0500

    volver a webhook

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import os
+from flask import Flask, request
 import telebot
 
 TOKEN = os.getenv('TOKEN')
+URL = os.getenv('URL_APP')
+
 bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=URL + TOKEN)
+    return "Bot funcionando correctamente", 200
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hola! Soy tu bot en Python.")
+    bot.reply_to(message, "¡Hola! Soy tu bot funcional en Python. ¿En qué puedo ayudarte?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, f"Me dijiste: {message.text}")
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
